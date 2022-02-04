800a2bd7e5907963c3b4cfc8b750d3b5ad27fd01
Marzan Islam <55044750+marzan999@users.noreply.github.com>
2022-02-04 18:42:30 +0600

    Create fun_pattern.py

---
+++ fun_pattern.py
@@ -0,0 +1,13 @@
+import turtle
+t = turtle.Turtle()
+list = ["green", 
+        "red", 
+        "purple", 
+        "blue", 
+        "cyan"]
+for i in range(200):
+        t.color(list[i%5])
+        t.pensize(i/10+1)
+        t.forward(i)
+        t.left(59)
+turtle.mainloop()
